e487fae83f27d68978df007859524ac5b8452e19
kiba518 <liujin_hzzh@126.com>
2019-06-13 09:48:22 +0800

    MarshalHelper
    MarshalHelper

--- MarshalHelper/Program.cs
+++ MarshalHelper/Program.cs
@@ -10,58 +10,37 @@ namespace MarshalHelper
     {
         static void Main(string[] args)
         {
-            IntPtr unsafePI = new IntPtr(); 
+            PointerUsage();
+
+            IntPtr unsafePI = new IntPtr();
             int testint = 518;
             unsafePI = MarshalHelper.Int32ToIntPtr(testint);
             int UnsaferetNoFree = Unsafe_Int32ToIntPtr_NoFree(unsafePI);
-            Console.WriteLine("Unsafe_Int32ToIntPtr_Free-取IntPtr的值" + UnsaferetNoFree); 
+            Console.WriteLine("Unsafe_Int32ToIntPtr_Free-取IntPtr的值" + UnsaferetNoFree);
+            new Task(() =>
+            {
+                int unsafeafterNoFree = MarshalHelper.IntPtrToInt32(unsafePI);
+                Console.WriteLine("Int32ToIntPtr_NoFree-未释放Intptr的线程取值" + unsafeafterNoFree);
+            }).Start();
+            Console.ReadKey();
 
             int retNoFree = Int32ToIntPtr_NoFree();
             IntPtr retNoFreeIP = new IntPtr(retNoFree);
             int retFree = Int32ToIntPtr_Free();
-            IntPtr retFreeIP = new IntPtr(retFree); 
+            IntPtr retFreeIP = new IntPtr(retFree);
 
-            new Task(()=> {
-                try
-                {
-                    int unsafeafterNoFree = MarshalHelper.IntPtrToInt32(unsafePI);
-                    Console.WriteLine("Int32ToIntPtr_NoFree-未释放Intptr的线程取值" + unsafeafterNoFree);
-                    int afterNoFree = MarshalHelper.IntPtrToInt32(retNoFreeIP);
-                    Console.WriteLine("Int32ToIntPtr_NoFree-未释放Intptr的线程取值" + afterNoFree);
-                    int afterFree = MarshalHelper.IntPtrToInt32(retFreeIP);
-                    Console.WriteLine("Int32ToIntPtr_Free-已释放Intptr的线程取值" + afterNoFree);
+            new Task(() =>
+            {
+                int afterNoFree = MarshalHelper.IntPtrToInt32(retNoFreeIP);
+                Console.WriteLine("Int32ToIntPtr_NoFree-未释放Intptr的线程取值" + afterNoFree);
+                int afterFree = MarshalHelper.IntPtrToInt32(retFreeIP);
+                Console.WriteLine("Int32ToIntPtr_Free-已释放Intptr的线程取值" + afterFree);
 
-                }
-                catch (Exception ex)
-                {
-
-                }
-                }).Start();
+            }).Start();
 
             Console.ReadKey();
 
-            string str = "I am Kiba518!";
-            int strlen = str.Length;
-            IntPtr sptr = MarshalHelper.StringToIntPtr(str);
-            unsafe
-            {
-                char* src = (char*)sptr.ToPointer();
-                //Console.WriteLine("地址" + (&src)); //报错
-                for (int i = 0; i <= strlen; i++)
-                {
-                    Console.Write(src[i]);
-                    src[i] = '0';
-                }
-                Console.WriteLine();
-                Console.WriteLine("========不安全代码改值========="); 
-                for (int i = 0; i <= strlen; i++)
-                {
-                    Console.Write(src[i]); 
-                }
-            }
-            Console.ReadKey(); 
-        }
-
+        } 
         #region SafeCode 安全代码
         static int Int32ToIntPtr_Free()
         {
@@ -97,5 +76,30 @@ namespace MarshalHelper
             }
         }
         #endregion
+
+        public static void PointerUsage()
+        {
+            string str = "I am Kiba518!";
+            int strlen = str.Length;
+            IntPtr sptr = MarshalHelper.StringToIntPtr(str);
+            unsafe
+            {
+                char* src = (char*)sptr.ToPointer();
+                //Console.WriteLine("地址" + (&src)); //这样写会报错，C#并不支持这样取指针地址
+                for (int i = 0; i <= strlen; i++)
+                {
+                    Console.Write(src[i]);
+                    src[i] = '0';
+                }
+                Console.WriteLine();
+                Console.WriteLine("========不安全代码改值=========");
+                for (int i = 0; i <= strlen; i++)
+                {
+                    Console.Write(src[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+
     }
 }
